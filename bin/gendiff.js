f5cec333df92c8df10c01edd4e8292a81c9a3e49
li9520 <li.dp9520@gmail.com>
2023-01-20 11:03:50 +0300

    модифицирован gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,11 @@ import { program } from 'commander';
 
 program
   .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  
   .description('Compares two configuration files and shows a difference.')
+  
   .parse(process.argv);
   
