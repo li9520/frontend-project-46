913237b40a724a4ebdaf96bb29a485508c28ea8a
li9520 <li.dp9520@gmail.com>
2023-01-24 20:00:33 +0300

    рефакторинг buildDiffTree

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -3,38 +3,39 @@ import _ from 'lodash';
 // BEGIN (write your solution here)
 
 const buildDiffTree  = (obj1, obj2) => {
-  const result = []
   const sortesKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-  for(const key of sortesKeys) {
+  return sortesKeys.map((key) => {
     const diff = {};
     diff.name = key;
-  
     if (!Object.hasOwn(obj1, key)) {
-      diff.status = 'added' ;
+      diff.status = 'added';
       diff.value = obj2[key];
+      return diff;
     }
-    else if (!Object.hasOwn(obj2, key)) {
+    if (!Object.hasOwn(obj2, key)) {
       diff.status = 'deleted';
       diff.value = obj1[key];
+      return diff;
     }
-    else if ((_.isObject(obj1[key])) && (_.isObject(obj2[key]))) {
-      diff.status ='unchanged' ;
+    if ((_.isObject(obj1[key])) && (_.isObject(obj2[key]))) {
+      diff.status = 'unchanged';
       diff.children = buildDiffTree(obj1[key], obj2[key]);
+      return diff;
     }
-    else if (obj1[key] === obj2[key]) {
-      diff.status ='unchanged' ;
+    if (obj1[key] === obj2[key]) {
+      diff.status = 'unchanged';
       diff.value = obj1[key];
+      return diff;
     }
     else {
-      diff.status = 'changed' ; 
+      diff.status = 'changed';
       diff.value = {
         old: obj1[key],
         new: obj2[key]
-      };
+      }
+      return diff;
     }
-    result.push(diff);
-  }
-  return result;
+  });
 };
 
 export default buildDiffTree;
