1156d7033dbc582a9a1fd28025dc8362cce947fb
li9520 <li.dp9520@gmail.com>
2023-01-25 13:16:43 +0300

    реализация тестов форматера plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,11 +11,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+let file1JSON, file2JSON, file1YML, file2YML;
+beforeAll(() => {
+  file1JSON = getFixturePath('nestedFile1.json');
+  file2JSON = getFixturePath('nestedFile2.json');
+  file1YML = getFixturePath('nestedFile1.yml');
+  file2YML = getFixturePath('nestedFile2.yml');
+});
 
 test('genDiff', () => {
-  const expectSimple = readFile('expectNested.txt');
-  const actual1 = genDiff(getFixturePath('nestedFile1.json'), getFixturePath('nestedFile2.json'));
-  expect(actual1).toEqual(expectSimple);
-  const actual2 = genDiff(getFixturePath('nestedFile1.yml'), getFixturePath('nestedFile2.yml'));
-  expect(actual2).toEqual(expectSimple);
+  expect(genDiff(file1JSON, file2JSON)).toEqual(readFile('expectNestedStylish.txt')) ;
+  expect(genDiff(file1YML, file2YML)).toEqual(readFile('expectNestedStylish.txt'));
+  expect(genDiff(file1JSON, file2JSON, 'plain')).toEqual(readFile ('expectNestedPlain.txt'));
+  expect(genDiff(file1YML, file2YML, 'plain')).toEqual(readFile('expectNestedPlain.txt'));
 });
